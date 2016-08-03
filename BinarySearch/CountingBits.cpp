790daa214d731c98f876f336ac6a0cf846f7d8af
fengxuan <651353997@qq.com>
2016-08-03 17:20:28 +0800

    some solution

---
+++ BinarySearch/CountingBits.cpp
@@ -0,0 +1,20 @@
+//
+// Created by fengxuan on 2016/8/3.
+//
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+
+    }
+};
+
+int main() {
+    cout << "Hello world" << endl;
+
+    return 0;
+}
